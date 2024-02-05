5a7ed0bbbd8664fe0b1e0a4a614fcaaeb90a6f36
yeonji-0120 <noyeonji43@gmail.com>
2024-02-05 16:53:32 +0900

    2024.02.05

---
+++ src/pages/pay/pay.js
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState, useRef } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { Navigate } from "react-router-dom";
+
+function pay(){
+
+    
+    return(
+        <div>급여관리 페이지</div>
+    );
+}
+
+export default pay;
