fb38c5c809604aefbfcac6ad9f3c93432f61e494
yeonji-0120 <noyeonji43@gmail.com>
2024-02-13 11:41:21 +0900

    2024.02.13 스케줄 진행중

---
+++ src/apis/ScheduleAPICalls.js
@@ -0,0 +1,28 @@
+import { POST_SCHEDULE } from "../modules/ScheduleModule";
+
+//전체 스케줄 조회
+export const callScheduleSearchAPI = ({ yearMonth }) => {
+  console.log("callScheduleSearchAPI Call");
+  const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/schedule/searchMonth`;
+  console.log("yearMonth", yearMonth);
+  return async (dispatch, getState) => {
+    const result = await fetch(requestURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        Authorization: "Bearer " + window.localStorage.getItem("accessToken"),
+      },
+      body: JSON.stringify({ yearMonth }),
+    }).then((response) => response.json());
+
+    console.log(
+      "[callScheduleSearchAPI] callScheduleSearchAPI RESULT : ",
+      result
+    );
+    if (result.status === 200) {
+      dispatch({ type: POST_SCHEDULE, payload: result.data });
+      console.log({ result });
+    }
+  };
+};
---
+++ src/modules/ScheduleModule.js
@@ -0,0 +1,20 @@
+import { createActions, handleActions } from "redux-actions";
+
+const initialState = [];
+
+export const POST_SCHEDULE = "schedule/POST_SCHEDULE";
+
+const actions = createActions({
+  [POST_SCHEDULE]: () => {},
+});
+
+const scheduleReducer = handleActions(
+  {
+    [POST_SCHEDULE]: (state, { payload }) => {
+      return payload;
+    },
+  },
+  initialState
+);
+
+export default scheduleReducer;
