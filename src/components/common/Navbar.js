80b51e49d67cf40a8d8e81ef968c8c00958daa7a
logwindy4 <logwindy4@gmail.com>
2024-02-13 14:55:17 +0900

    Login 화면작업

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -13,70 +13,98 @@ import setting from '../../@core/img/icons/unicons/setting.png';
 import pay from '../../@core/img/icons/unicons/pay.png';
 import organization from '../../@core/img/icons/unicons/organization .png';
 
+
+
 function Navbar() {
-    return (
-        <div
-            id='layout-menu'
-            className={`${coreCSS[`layout-menu`]} ${coreCSS[`menu-vertical`]} ${coreCSS[`menu`]} ${
-                themDefaultCSS[`bg-menu-theme`]
-            }`}
-        >
-            <NavLink to='/' className={`${coreCSS[`app-brand-link`]}`}>
-                <img src={logo} style={{ paddingTop: 30, paddingBottom: 30 }} />
-            </NavLink>
-            <div className={`${coreCSS[`menu-inner-shadow`]}`} />
-            <ul className={`${coreCSS[`menu-inner`]} ${coreCSS[`py-1`]}`}>
-                <li className={`${coreCSS[`menu-item`]}`}>
-                    <NavLink to='/' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={home} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>홈</div>
-                    </NavLink>
-                </li>
-                <li className={`${coreCSS[`menu-item`]}`}>
-                    <NavLink to='/schedule' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={schedule} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>스케줄</div>
-                    </NavLink>
-                </li>
-                <li className={`${coreCSS[`menu-item`]}`}>
-                    <NavLink to='/approval' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={approval} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>결재</div>
-                    </NavLink>
-                </li>
-                <li className={`${coreCSS[`menu-item`]}`}>
-                    <NavLink to='/organizationChart' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={organization} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>부서</div>
-                    </NavLink>
-                </li>
-                <li className={`${coreCSS[`menu-item`]}`}>
-                    <NavLink to='/notice' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={notice} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>공지</div>
-                    </NavLink>
-                </li>
-                <li className={`${coreCSS[`menu-item`]}`}>
-                    <NavLink to='/dataformat' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={document} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>파일함</div>
-                    </NavLink>
-                </li>
-                <li className={`${coreCSS[`menu-item`]} `}>
-                    <NavLink to='/pay' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={pay} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>급여관리</div>
-                    </NavLink>
-                </li>
-                <li className={`${coreCSS[`menu-item`]}`}>
-                    <NavLink to='/setting' className={`${coreCSS[`menu-link`]}`}>
-                        <img src={setting} style={{ width: 40 }} />
-                        <div style={{ paddingLeft: 10 }}>설정</div>
-                    </NavLink>
-                </li>
-            </ul>
+
+    const isLogin = window.localStorage.getItem('accessToken');
+
+    function BeforeLogin() {
+
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+    
+    function AfterLogin() {
+    
+
+        return (
+        <div>
+            <div
+                id='layout-menu'
+                className={`${coreCSS[`layout-menu`]} ${coreCSS[`menu-vertical`]} ${coreCSS[`menu`]} ${
+                    themDefaultCSS[`bg-menu-theme`]
+                }`}
+            >
+                <NavLink to='/' className={`${coreCSS[`app-brand-link`]}`}>
+                    <img src={logo} style={{ paddingTop: 30, paddingBottom: 30 }} />
+                </NavLink>
+                <div className={`${coreCSS[`menu-inner-shadow`]}`} />
+                <ul className={`${coreCSS[`menu-inner`]} ${coreCSS[`py-1`]}`}>
+                    <li className={`${coreCSS[`menu-item`]}`}>
+                        <NavLink to='/' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={home} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>홈</div>
+                        </NavLink>
+                    </li>
+                    <li className={`${coreCSS[`menu-item`]}`}>
+                        <NavLink to='/schedule' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={schedule} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>스케줄</div>
+                        </NavLink>
+                    </li>
+                    <li className={`${coreCSS[`menu-item`]}`}>
+                        <NavLink to='/approval' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={approval} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>결재</div>
+                        </NavLink>
+                    </li>
+                    <li className={`${coreCSS[`menu-item`]}`}>
+                        <NavLink to='/organizationChart' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={organization} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>부서</div>
+                        </NavLink>
+                    </li>
+                    <li className={`${coreCSS[`menu-item`]}`}>
+                        <NavLink to='/notice' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={notice} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>공지</div>
+                        </NavLink>
+                    </li>
+                    <li className={`${coreCSS[`menu-item`]}`}>
+                        <NavLink to='/dataformat' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={document} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>파일함</div>
+                        </NavLink>
+                    </li>
+                    <li className={`${coreCSS[`menu-item`]} `}>
+                        <NavLink to='/pay' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={pay} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>급여관리</div>
+                        </NavLink>
+                    </li>
+                    <li className={`${coreCSS[`menu-item`]}`}>
+                        <NavLink to='/setting' className={`${coreCSS[`menu-link`]}`}>
+                            <img src={setting} style={{ width: 40 }} />
+                            <div style={{ paddingLeft: 10 }}>설정</div>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         </div>
-    );
+        );
+    }
+        return(
+            <>
+            {isLogin == null || isLogin === undefined ? <BeforeLogin /> : <AfterLogin />}
+            </>
+        );
+
+    
+
 }
 
 export default Navbar;
